f3932d6066f28cf1eec0aa2599846a3fa981b8c5
Sunny Vats <layers_tipsier_0n@icloud.com>
2024-07-03 20:31:24 +0530

    Modify project - 03-July
     - Add changes to pom to configure compiler argument for mapstruct.
     - Add listAllProduct from fakeStore API implementation.
     - Add RestTemplate instance creation using @Bean.
     - Add example of @Configuration class.

--- src/main/java/com/dev/sunny/configs/AppConfig.java
+++ src/main/java/com/dev/sunny/configs/AppConfig.java
@@ -1,16 +1,16 @@
 package com.dev.sunny.configs;
 
-import com.dev.sunny.mappers.ProductMapper;
-import com.dev.sunny.mappers.ProductMapperImpl;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    ProductMapper productMapper() {
-        return new ProductMapperImpl();
+    public RestTemplate restTemplate() {
+        return new RestTemplateBuilder().build();
     }
 
 }
--- src/main/java/com/dev/sunny/controllers/ProductController.java
+++ src/main/java/com/dev/sunny/controllers/ProductController.java
@@ -10,21 +10,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
+import java.util.List;
+
 @RestController
 @RequestMapping("/products/api")
 @RequiredArgsConstructor
 public class ProductController {
 
     private final ProductMapper mapper;
+    private final RestTemplate restTemplate;
 
     @GetMapping("/{id}")
     public Product productByID(@PathVariable("id") Long id) {
-        RestTemplate restTemplate = new RestTemplate();
         ProductResponseDTO fetchedProduct = restTemplate.getForObject("https://fakestoreapi.com/products/" + id, ProductResponseDTO.class);
 
         if (fetchedProduct == null) return new Product();
 
         return mapper.productResponseDTOToProduct(fetchedProduct);
+    }
+
+    @GetMapping("/all")
+    public List<Product> listAllProduct() {
+        ProductResponseDTO[] productArray = restTemplate.getForObject("https://fakestoreapi.com/products", ProductResponseDTO[].class);
+        List<ProductResponseDTO> fetchedProducts = productArray != null ? List.of(productArray) : Collections.emptyList();
+
+        return fetchedProducts.stream()
+                .map(mapper::productResponseDTOToProduct)
+                .toList();
     }
 
 }
