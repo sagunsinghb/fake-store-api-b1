15ed5845822c82798d452d874500cb0f9901514c
Sunny Vats <layers_tipsier_0n@icloud.com>
2024-07-13 16:40:59 +0530

    Add init demo project.
     - Add source and output encoding property to pom.xml.
     - Add spring boot starter web, jpa dependencies.
     - Add H2 dependency for in-mem database.
     - Add devtools for dev purposes(restart on source code change).
     - Add lombok dependency to reduce clutter from source POJOs.

--- src/main/java/com/dev/sunny/controllers/CategoryController.java
+++
@@ -1,9 +0,0 @@
-package com.dev.sunny.controllers;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/categories/api")
-public class CategoryController {
-}
