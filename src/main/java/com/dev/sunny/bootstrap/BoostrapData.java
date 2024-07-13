0e7a4439e735d5540022113dcf2a9e4e24c2346a
Sunny Vats <layers_tipsier_0n@icloud.com>
2024-07-13 21:17:53 +0530

    Add Initial Data to repository using spring provided CommandLineRunner interface.

---
+++ src/main/java/com/dev/sunny/bootstrap/BoostrapData.java
@@ -0,0 +1,33 @@
+package com.dev.sunny.bootstrap;
+
+import com.dev.sunny.model.Book;
+import com.dev.sunny.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class BoostrapData implements CommandLineRunner {
+
+    private final BookRepository bookRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Book b1, b2;
+        b1 = new Book("The Alchemist", "9780062315007", "Paulo Coelho");
+        b2 = new Book("The Da Vinci Code", "9780307474278", "Dan Brown");
+
+        try {
+            bookRepository.save(b1);
+            bookRepository.save(b2);
+        } catch (IllegalArgumentException e) {
+            throw new Exception(e);
+        }
+
+        System.out.println("Books saved: " + bookRepository.count());
+        bookRepository.findAll()
+                .forEach(System.out::println);
+
+    }
+}
